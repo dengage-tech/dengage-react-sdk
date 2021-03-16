71c6bd3ba4a042efaa7aad1ea07a8f187d3104db
Kamran Younis <join.kamran92@gmail.com>
2021-03-16 19:39:44 +0500

    setup changes in code for screenshots etc

--- example/android/app/src/main/java/com/example/reactnativedengage/MainActivity.java
+++ example/android/app/src/main/java/com/example/reactnativedengage/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.reactnativedengage;
 
+import android.content.Context;
+import android.os.Bundle;
+
+import com.dengage.sdk.DengageManager;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -12,4 +16,17 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "DengageExample";
   }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    Context context = getApplicationContext();
+    final DengageManager manager = DengageManager .getInstance(context)
+      .setLogStatus(true)
+      .setFirebaseIntegrationKey("YOUR_DENGAGE_FIREBASE_APP_INTEGRATION_KEY")
+      .setHuaweiIntegrationKey("OUR_DENGAGE_HUAWEI_APP_INTEGRATION_KEY")
+      .init();
+
+  }
+
 }
---
+++ example/ios/DengageNotificationServiceExtension/NotificationService.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationService.swift
+//  DengageNotificationServiceExtension
+//
+//  Created by Kamran Younis on 15/03/2021.
+//
+
+import UserNotifications
+import Dengage
+
+class NotificationService: UNNotificationServiceExtension {
+
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+
+  override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+    self.contentHandler = contentHandler
+    bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+     
+    if let bestAttemptContent = bestAttemptContent {
+        
+        // add this line of code
+        Dengage.didReceiveNotificationExtentionRequest(receivedRequest: request, with: bestAttemptContent)
+        contentHandler(bestAttemptContent)
+    }
+}
