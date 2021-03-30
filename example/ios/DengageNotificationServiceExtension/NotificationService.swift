237454d0188b77f484bd8d3659a1c6cdedc08ff9
Kamran Younis <join.kamran92@gmail.com>
2021-03-30 17:38:54 +0500

    rich notification service build successful

--- example/ios/DengageNotificationServiceExtension/NotificationService.swift
+++ example/ios/DengageNotificationServiceExtension/NotificationService.swift
@@ -2,26 +2,30 @@
 //  NotificationService.swift
 //  DengageNotificationServiceExtension
 //
-//  Created by Kamran Younis on 15/03/2021.
+//  Created by Macbook Pro on 30/03/2021.
 //
 
 import UserNotifications
-//import Dengage
+import Dengage_Framework
 
 class NotificationService: UNNotificationServiceExtension {
 
     var contentHandler: ((UNNotificationContent) -> Void)?
     var bestAttemptContent: UNMutableNotificationContent?
 
-//  override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
-//    self.contentHandler = contentHandler
-//    bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-//
-//    if let bestAttemptContent = bestAttemptContent {
-//
-//        // add this line of code
-//        Dengage.didReceiveNotificationExtentionRequest(receivedRequest: request, with: bestAttemptContent)
-//        contentHandler(bestAttemptContent)
-//    }
-//}
+      override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+          self.contentHandler = contentHandler
+          bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+          if let bestAttemptContent = bestAttemptContent {
+            Dengage.didReceiveNotificationExtentionRequest(receivedRequest: request, withNotificationContent: bestAttemptContent)
+            contentHandler(bestAttemptContent)
+          }
+      }
+    override func serviceExtensionTimeWillExpire() {
+        // Called just before the extension will be terminated by the system.
+        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
 }
