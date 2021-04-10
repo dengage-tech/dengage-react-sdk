a0663c27fdfa918a58c6d70a4f650d89c39c4ea9
Kamran Younis <join.kamran92@gmail.com>
2021-04-10 22:10:51 +0500

    better ui example, android push issue debug

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,15 +1,13 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text, Platform, Button, TextInput } from 'react-native';
+import { Button, Dimensions, Platform, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 import Dengage from 'react-native-dengage';
-import { DengageTypes } from '../../src/types';
 import { useFocusEffect, useNavigation } from '@react-navigation/native';
-import { BaseButton } from 'react-native-gesture-handler';
 
 export default function App() {
-  const [result, setResult] = React.useState<string>('checking...');
-  const [token, setToken] = React.useState<string>('checking...');
   const [contactKey, setContactKey] = React.useState<string>('checking...');
+  const [subscription, setSubscription] = React.useState<string>('');
+  const [subscriptionHidden, toggleSubscription] = React.useState<string>(false);
 
   const pageViewExample = () => {
     // pageView event example
@@ -89,7 +87,6 @@ export default function App() {
     cartItems.push(cartItem)
     Dengage.viewCart(cartItems)
   }
-
   const appInboxExamples = async () => {
     const inboxMessages = await Dengage.getInboxMessages(10, 20)
       .catch(err => err)
@@ -106,53 +103,22 @@ export default function App() {
   }
 
   React.useEffect(() => {
+    Dengage.setLogStatus(true);
     if (Platform.OS === 'ios') {
-      Dengage.promptForPushNotificationsWitCallback(async (hasPermission) => {
-        console.log("hasPermission", hasPermission)
-        Dengage.setUserPermission(hasPermission)
-        setResult(String(hasPermission))
+      Dengage.promptForPushNotificationsWitCallback(hasPermission => {
+        Dengage.setUserPermission(Boolean(hasPermission))
         if (hasPermission) {
-          const token = await Dengage.getToken()
-          console.log("tokenIs: ", token)
-          setToken(token)
-          if (token) {
-            Dengage.setToken(token)
-            Dengage.setLogStatus(true);
-          }
-          setContactKey(await Dengage.getContactKey())
-
-          pageViewExample()
-          addToCartExample()
-          removeFromCartExample()
-          appInboxExamples()
+          init()
         }
       })
     } else {
-      const invokeIt = async () => {
-        const hasPermissions = await Dengage.getUserPermission()
-        setResult(String(hasPermissions))
-        const token = await Dengage.getToken()
-        setToken(token);
-        setContactKey((await Dengage.getContactKey()) ?? "")
-        const subscription = await Dengage.getSubscription()
-        console.log("subscription", subscription)
-        pageViewExample()
-        addToCartExample()
-        // removeFromCartExample()
-        appInboxExamples()
-      }
-      invokeIt()
+      init()
     }
+  }, [])
 
-
-    if (Platform.OS === 'ios') {
-      Dengage.handleNotificationActionBlock((notificationAction: DengageTypes["NotificationAction"]) => {
-        console.log(notificationAction.notification)
-        // handle notification {notificationAction} here.
-      })
-    }
-
-  }, []);
+  const init = async () => {
+    setContactKey(await Dengage.getContactKey())
+  }
 
   const navigation = useNavigation()
   useFocusEffect(
@@ -162,24 +128,40 @@ export default function App() {
   );
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.heading}>HasPermission</Text>
-      <Text>{result}</Text>
-      <Text style={styles.heading}>Token</Text>
-      <Text>{token}</Text>
-
-      <Text>Enter Contact Key:</Text>
-      <TextInput
-        value={contactKey}
-        onChangeText={(val) => setContactKey(val)}
-        style={styles.input}/>
+    <ScrollView
+      contentContainerStyle={styles.contentContainerStyles}
+      style={styles.container}>
 
       <View style={styles.btnContainer}>
         <Button
           onPress={async () => {
-            await Dengage.setContactKey(contactKey)
+            const hasPermissions = await Dengage.getUserPermission()
+            alert("hasPermissions: " + hasPermissions)
           }}
-          title={"update Contact Key"}
+          title={"check Permission"}
+        />
+      </View>
+
+      {
+        Platform.OS === 'ios' && <View style={styles.btnContainer}>
+          <Button
+            onPress={() => {
+              Dengage.promptForPushNotificationsWitCallback(async (hasPermission) => {
+                Dengage.setUserPermission(hasPermission)
+              })
+            }}
+            title={"ask Permission"}
+          />
+        </View>
+      }
+
+      <View style={styles.btnContainer}>
+        <Button
+          onPress={async () => {
+            const token = await Dengage.getToken()
+            alert('token: ' + token)
+          }}
+          title={"get & show token"}
         />
       </View>
 
@@ -192,6 +174,37 @@ export default function App() {
         />
       </View>
 
+      <View style={styles.groupContainer}>
+        <Text>Edit/Enter Contact Key to update:</Text>
+        <TextInput
+          value={contactKey}
+          onChangeText={(val) => setContactKey(val)}
+          style={styles.input}/>
+
+        <View style={styles.btnContainer}>
+          <Button
+            onPress={async () => {
+              await Dengage.setContactKey(contactKey)
+            }}
+            title={"update Contact Key"}
+          />
+        </View>
+      </View>
+
+      <View style={styles.btnContainer}>
+        <Button
+          onPress={async () => {
+            if (!subscription) {
+              setSubscription(await Dengage.getSubscription())
+            } else {
+              toggleSubscription(!subscriptionHidden)
+            }
+          }}
+          title={'Show/Hide Current Subscription'}
+        />
+        {!subscriptionHidden && <Text>{`subscription: ${subscription}`}</Text>}
+      </View>
+
       <View style={styles.btnContainer}>
         <Button
           onPress={() => navigation.navigate('SecondScreen')}
@@ -199,15 +212,16 @@ export default function App() {
         />
       </View>
       <Text>Note: On Navigation to second screen, setNavigationWithName is called.</Text>
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  container: {},
+  contentContainerStyles: {
+    width: Dimensions.get('window').width,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'stretch',
     padding: 20,
   },
   box: {
@@ -226,5 +240,10 @@ const styles = StyleSheet.create({
   },
   btnContainer: {
     margin: 10
+  },
+  groupContainer: {
+    borderWidth: 1,
+    borderColor: 'grey',
+    padding: 10
   }
 });
