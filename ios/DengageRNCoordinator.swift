7a285986fcf48938298452d689b9fed6d2669428
hasnain <hasnain.tanvir@dengage.com>
2023-11-29 02:15:45 +0500

    libraries updated

--- ios/DengageRNCoordinator.swift
+++ ios/DengageRNCoordinator.swift
@@ -37,7 +37,7 @@ public class DengageRNCoordinator: NSObject {
         {
             Dengage.requestLocationPermissions()
         }
-        
+        Dengage.setHybridAppEnvironment()
     }
     
     @objc(registerForPushToken:)
