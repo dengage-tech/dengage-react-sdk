1fa44a67eeb7744c0416835d1937cd04e501af81
hasnainTanvir <hasnain.tanvir@dengage.com>
2024-05-08 21:21:39 +0500

    set development status exposed

--- ios/Dengage.swift
+++ ios/Dengage.swift
@@ -493,7 +493,7 @@ class DengageRN: RCTEventEmitter {
             resolve(deviceId)
         } catch {
             print("Unexpected getDeviceId error: \(error)")
-            reject("UNABLE_TO_RETREIVE_DEVICE_ID, error.localizedDescription ?? "Something went wrong", error)
+            
         }
     }
 
