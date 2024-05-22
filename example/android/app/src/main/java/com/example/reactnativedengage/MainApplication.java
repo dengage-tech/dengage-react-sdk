75f1ee1a98d6df50b80af45b014baba408541683
hasnainTanvir <hasnain.tanvir@dengage.com>
2024-05-23 02:02:54 +0500

    InApp inline implemented

--- example/android/app/src/main/java/com/example/reactnativedengage/MainApplication.java
+++ example/android/app/src/main/java/com/example/reactnativedengage/MainApplication.java
@@ -8,6 +8,8 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.soloader.SoLoader;
+import com.reactnativedengage.InAppInlinePackage;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -26,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for DengageExample:
           // packages.add(new MyReactNativePackage());
+          packages.add(new InAppInlinePackage());
           return packages;
         }
 
