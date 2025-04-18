72769e8a5bd4fa77bf61f1dcfad96c4937c38192
egemen1986 <egemen.gulkilik@dengage.com>
2025-04-18 19:17:08 +0300

    delete old files

--- example/index.tsx
+++
@@ -1,6 +0,0 @@
-import 'react-native-gesture-handler';
-import { AppRegistry } from 'react-native';
-import Navigation from './src/Navigation';
-import { name as appName } from './app.json';
-
-AppRegistry.registerComponent(appName, () => Navigation);
--- example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  DengageExample
-//
-
-import Foundation
