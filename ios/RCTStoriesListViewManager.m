c9aaa1c49a2f85175ecbf7557c7d3aca0e582660
egemen1986 <egemen.gulkilik@dengage.com>
2025-12-19 16:07:21 +0300

    fix import in ios view managers

--- ios/RCTStoriesListViewManager.m
+++ ios/RCTStoriesListViewManager.m
@@ -10,7 +10,11 @@
 #import <React/RCTBridge.h>
 #import <React/RCTUIManager.h>
 
+#if __has_include("react_native_dengage/react_native_dengage-Swift.h")
+#import "react_native_dengage/react_native_dengage-Swift.h"
+#else
 #import "react_native_dengage-Swift.h"
+#endif
 
 @implementation RCTStoriesListViewManager
 
