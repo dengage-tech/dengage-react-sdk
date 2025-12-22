13190092480d7eadfcce7d0a22fcadc51694e604
egemen1986 <egemen.gulkilik@dengage.com>
2025-12-22 18:55:27 +0300

    update integrationKey in example app

--- example/ios/ReactNativeDengageExample/DengageCoordinatorHelper.m
+++ example/ios/ReactNativeDengageExample/DengageCoordinatorHelper.m
@@ -11,10 +11,10 @@
 @implementation DengageCoordinatorHelper
 
 //dev-app.dengage.com: ios-rn-sandbox-test
-//NSString *integrationKey = @"RtWFz7e1SIvcYm3IeaPg6mWwtSZS_p_l_uZUZjQ5H5PYMphJDQaj_p_l_2x_p_l_sygPdd8k4OnwDfKfxy8e6zfLezdFrlmUAVDz1o2avZsolsEgQq3eDJy_p_l_TLRRjHT2wzGZwPkbmTla5_p_l_uM4299j2Jde4iNO9MLcA_e_q__e_q_";
+NSString *integrationKey = @"RtWFz7e1SIvcYm3IeaPg6mWwtSZS_p_l_uZUZjQ5H5PYMphJDQaj_p_l_2x_p_l_sygPdd8k4OnwDfKfxy8e6zfLezdFrlmUAVDz1o2avZsolsEgQq3eDJy_p_l_TLRRjHT2wzGZwPkbmTla5_p_l_uM4299j2Jde4iNO9MLcA_e_q__e_q_";
 
 //dev-app.dengage.com: ios-rn-testflight-test
-NSString *integrationKey = @"n_p_l_lO9FMM4fzlX4jbudL355PGRpKsipGVHMgaPbOGAxR_s_l_MJo3J1KWkHYAz4u93U09KjPXZeVqZAZpC_s_l_JjJSlGPBWiHO2pIFsMG71SR5_p_l_YMNTnN1_p_l_Aje3MXqKCRpYjEUBNroiGTGF609W8EhEcRaQAZA_e_q__e_q_";
+//NSString *integrationKey = @"n_p_l_lO9FMM4fzlX4jbudL355PGRpKsipGVHMgaPbOGAxR_s_l_MJo3J1KWkHYAz4u93U09KjPXZeVqZAZpC_s_l_JjJSlGPBWiHO2pIFsMG71SR5_p_l_YMNTnN1_p_l_Aje3MXqKCRpYjEUBNroiGTGF609W8EhEcRaQAZA_e_q__e_q_";
 
 
 + (void)registerDeviceToken:(NSData *)deviceToken {
